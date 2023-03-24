87eed2b7c53ab7862959f0b10877e3d11e811b7e
danangamw <danangamw@gmail.com>
2023-03-24 10:23:34 +0700

    add auth and roles, restrict admin route to admin only

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AdminHomeController;
 use App\Http\Controllers\AdminProductController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,9 +23,13 @@ Route::get('/about', [HomeController::class, 'about']);
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/product/{id}', [ProductController::class, 'show']);
 
-Route::get('/admin', [AdminHomeController::class, 'index']);
-Route::get('/admin/product', [AdminProductController::class, 'index']);
-Route::post('/admin/product/store', [AdminProductController::class, 'store']);
-Route::delete('/admin/product/{id}/delete', [AdminProductController::class, 'delete']);
-route::get('/admin/product/{id}/edit', [AdminProductController::class, 'edit']);
-route::put('/admin/product/{id}/update', [AdminProductController::class, 'update']);
+Route::middleware('admin')->group(function () {
+    Route::get('/admin', [AdminHomeController::class, 'index']);
+    Route::get('/admin/product', [AdminProductController::class, 'index']);
+    Route::post('/admin/product/store', [AdminProductController::class, 'store']);
+    Route::delete('/admin/product/{id}/delete', [AdminProductController::class, 'delete']);
+    route::get('/admin/product/{id}/edit', [AdminProductController::class, 'edit']);
+    route::put('/admin/product/{id}/update', [AdminProductController::class, 'update']);
+});
+
+Auth::routes();
